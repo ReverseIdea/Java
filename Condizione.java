8482ee4fb6d44f50359da1cea79bfac65af4016a
ReverseIdea <cirella.armando@gmail.com>
2025-07-19 17:27:48 +0200

    cond_

---
+++ Condizione.java
@@ -0,0 +1,17 @@
+public class Condizione {
+	public static void main ( String [] args) {
+		int eta=25;
+		if (eta<15){
+			System.out.println ("sei bambino");
+	} else if (eta<20){
+		System.out.println ("sei un teeneger");
+	} else if (eta<30){
+		System.out.println ("sei un giovane adulto");
+	} else if (eta<60){
+		System.out.println ("sei un adulto");
+	} else{
+		System.out.println ("sei un senior");
+	}
+	}
+	
+}
