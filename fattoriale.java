e73e9c10275d5a46fd4da67064f914d5486ec621
ReverseIdea <cirella.armando@gmail.com>
2025-07-19 17:28:43 +0200

    Fatt_

---
+++ fattoriale.java
@@ -0,0 +1,18 @@
+public class fattoriale {
+	public static void main (String [] args) {
+		int a=8, fattoriale=1;
+		while (a>=1){
+			fattoriale*=a;
+			a--;
+		}
+		System.out.println (fattoriale);
+	
+	int b=0;
+	while (b<=40){
+		if (b%2==0);
+		System.out.println (b);
+		}
+		b++;
+	
+	}
+}
