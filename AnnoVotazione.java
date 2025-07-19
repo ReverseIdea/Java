00b995ba030e510578518ceff8d6af528fde1c21
ReverseIdea <cirella.armando@gmail.com>
2025-07-19 17:26:32 +0200

    Ann_vot

---
+++ AnnoVotazione.java
@@ -0,0 +1,14 @@
+public class AnnoVotazione {
+	public static void main (String [] args){
+		int eta=26;
+		int anni=17;
+		if (eta>anni){
+			System.out.println(eta + "puo' votare");
+		}else{
+			System.out.println(eta + "non puo' votare");
+		}
+		
+		
+		
+		}
+}
