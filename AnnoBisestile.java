18da122ab83b13531d0a199e8fc75e642b6b30e1
ReverseIdea <cirella.armando@gmail.com>
2025-07-19 17:18:43 +0200

    Anno_Bi

---
+++ AnnoBisestile.java
@@ -0,0 +1,13 @@
+public class AnnoBisestile {
+	public static void main (String [] args){
+		int anno=2017;
+		if (anno%4==0){
+			System.out.println(anno +" e' bisestile");
+		}else{
+			System.out.println(anno +" non e' bisestile");
+		}
+		
+		
+		
+		}
+}
