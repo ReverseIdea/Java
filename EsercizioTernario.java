732517b1f2b9b8f4433fe2b97d0a8c52f0ef2fa4
ReverseIdea <cirella.armando@gmail.com>
2025-07-19 17:29:05 +0200

    Ternario

---
+++ EsercizioTernario.java
@@ -0,0 +1,9 @@
+public class EsercizioTernario {
+	public static void main (String [] args) {
+		int eta= 15;
+		String voto;
+		voto=(eta>=18)?"si puo' votare":"Non puo' votare";
+		System.out.println (voto);
+		
+	}
+}
