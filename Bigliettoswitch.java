153ebbae9b96f2c0b16e49856a2ecad04eba26d0
ReverseIdea <cirella.armando@gmail.com>
2025-07-19 17:27:24 +0200

    Biglietto_switch

---
+++ Bigliettoswitch.java
@@ -0,0 +1,23 @@
+public class Bigliettoswitch {
+	public static void main (String [] args){
+	int eta=63;
+	switch (eta) {
+		case 6:
+			System.out.println (eta "Entra Gratis");
+		break;
+		case 17:
+			System.out.println (eta "paga 10 euro");
+		break;
+		case 54:
+			System.out.println (eta "Paga 15 euro");
+		break;
+		case 60:
+			System.out.println (eta "Paga 7 Euro");
+		break;
+		case 70:
+			System.out.println (eta"Paga 5 Euro");
+		break;
+				
+		}
+	}
+}
